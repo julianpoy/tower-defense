ab284c2ad35829b2b90c038bfd6797178698fdbf
Julian Poyourow <julianpoyo@gmail.com>
2020-08-30 11:01:01 -0700

    Add index.html

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,16 @@
+import copy from 'rollup-plugin-copy';
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/bundle.js',
     format: 'cjs'
-  }
+  },
+  plugins: [
+    copy({
+      targets: [
+        { src: 'src/index.html', dest: 'dist' },
+      ]
+    })
+  ]
 };
